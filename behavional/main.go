4abec705cd149348b7459853a07b9c5f8bc3e5d2
takochuu <takochuu@gmail.com>
2017-09-10 10:22:08 +0900

    first commit

---
+++ behavional/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type (
+	Event struct {
+		Data int64
+	}
+
+	Observer interface {
+		OnNotify(Event)
+	}
+
+	Notifier interface {
+		Register(Observer)
+		Deregister(Observer)
+		Notify(Event)
+	}
+)
+
+type eventObserver struct {
+	id int
+}
+
+type eventNotifier struct {
+	observers map[Observer]struct{}
+}
+
+func (o *eventObserver) OnNotify(e Event) {
+	fmt.Printf("*** Observer %d received: %d\n", o.id, e.Data)
+}
+
+func (o *eventNotifier) Register(l Observer) {
+	o.observers[l] = struct{}{}
+}
+
+func (o *eventNotifier) Deregister(l Observer) {
+	delete(o.observers, l)
+}
+
+func (p *eventNotifier) Notify(e Event) {
+	for o := range p.observers {
+		o.OnNotify(e)
+	}
+}
+
+func main() {
+	n := eventNotifier{
+		observers: map[Observer]struct{}{},
+	}
+
+	n.Register(&eventObserver{id: 1})
+	n.Register(&eventObserver{id: 2})
+
+	stop := time.NewTimer(10 * time.Second).C
+	tick := time.NewTicker(time.Second).C
+	for {
+		select {
+		case <-stop:
+			return
+		case t := <-tick:
+			n.Notify(Event{Data: t.UnixNano()})
+		}
+	}
+}
